d146f1e1e0c19a927d9fefccb33e63c420768010
AdamWsk <adam.wosiek@gmail.com>
2022-01-25 17:05:34 +0100

    instrukcja warunkowa else if

---
+++ src/instrukcjaWarunkowaElseIf.java
@@ -0,0 +1,13 @@
+public class instrukcjaWarunkowaElseIf {
+    public static void main(String[] args) {
+        int a = -1;
+
+        if (a == 0) {
+            System.out.println("Liczba jest równa 0");
+        } else if (a>0) {
+            System.out.println("Liczba jest większa od 0");
+        } else if (a<0) {
+            System.out.println("Liczba jest mniejsza od 0");
+        }
+    }
+}
