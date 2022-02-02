aed824b68cf5a8f1b7fc9eb1e051de74800edc1a
AdamWsk <adam.wosiek@gmail.com>
2022-02-02 17:42:05 +0100

    praca domowa petle

---
+++ src/PracaDomowaPetle.java
@@ -0,0 +1,17 @@
+public class PracaDomowaPetle {
+    public static void main(String[] args) {
+
+        int zmienna = 10;
+
+        for (int i=0; i<zmienna; i++) {
+            if (i%3==0) {
+                System.out.println("Liczba: " + i + " jest podzielna przez 3.");
+            }
+        }
+
+        int[] numbers = new int[] {1,2,3,4,5};
+
+        for (int i=0; 2<(numbers.length/2); i++);
+
+    }
+}
