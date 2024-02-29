c2dfef0f9eba85253c78cfda85cdf77242b8176b
AdamWsk <adam.wosiek@gmail.com>
2024-02-29 19:13:37 +0100

    typy proste

---
+++ src/typyProste.java
@@ -0,0 +1,22 @@
+public class typyProste {
+
+    //liczby stalorzecinkowe
+    byte liczba1 = 127; // 1 bajt - zakres od -128 do 127
+    short liczba2 = 32689; // 2 bajty - zakres od -32 768 do 32 767
+    int liczba3 = 32768999; // 4 bajty - zakres od -2 147 483 648 do 2 147 483 647
+    long liczba4 = 4444444444L; //- 8 bajtów - zakres od -2^63 do (2^63)-1 (posiadają przyrostek L , lub l)
+
+    //liczby zmiennoprzecinkowe
+    float liczba5 = 4.99993F; // 4 bajty, max 6 liczb po przecinku
+    double liczba6 =  3.99999999999999999D; // 8 bajtow - do 15 po przecinku
+
+    //wartosci logiczne
+    boolean prawda = true;
+    boolean falsz = false;
+
+    //pojedynczy znak
+    char letter = 'A';
+
+
+}
+
